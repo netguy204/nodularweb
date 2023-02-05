b8cd45f822fba2c31479f2b09ee1a5c7273c021e
Brian Taylor <brian.taylor@optimizely.com>
2023-02-04 21:30:59 -0500

    use consistent port

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
-const port = 4000
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello, smokey world' })
